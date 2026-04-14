b8668484b98bdf4218ea4a3f3edc481cffec473f
Mako Kamiya <mako4kamiya@gmail.com>
2026-04-14 13:24:37 +0900

    Google Font "Libre Baskerville"の読み込み

--- functions.php
+++ functions.php
@@ -19,8 +19,17 @@
 			wp_enqueue_style('main-style', get_template_directory_uri() . '/assets/css/main.css');
 			wp_enqueue_style('header-style', get_template_directory_uri() . '/assets/css/header.css');
 			wp_enqueue_style('front-page-style', get_template_directory_uri() . '/assets/css/front-page.css');
+			wp_enqueue_style('Libre Baskerville', 'https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400..700;1,400..700&display=swap');
 		}
 	endif;
 	add_action('wp_enqueue_scripts', 'my_split_styles');
+
+	if ( ! function_exists( 'my_theme_add_preconnect' ) ) :
+		function my_theme_add_preconnect() {
+			echo '<link rel="preconnect" href="https://fonts.googleapis.com">';
+			echo '<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>';
+		}
+	endif;
+	add_action('wp_head', 'my_theme_add_preconnect');
 
 ?>
