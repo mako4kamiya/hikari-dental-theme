2164fddd826bc51eb2cee92c0d8de21c96ca0294
Mako Kamiya <mako4kamiya@gmail.com>
2026-04-16 11:23:39 +0900

    ヘッダーにアニメーションを追加

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,1 +1,14 @@
-console.log("header.js is loaded");
+const Header = document.querySelector('header');
+const MainVisual = document.querySelector('#MainVisual');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (!entry.isIntersecting) {
+            Header.classList.add('is-show');
+        } else {
+            Header.classList.remove('is-show');
+        }
+    });
+});
+
+observer.observe(MainVisual);
