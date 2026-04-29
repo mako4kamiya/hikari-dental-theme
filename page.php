ad9fa26f33db80a13c3cd1168dac1ccf642c03d7
Mako Kamiya <mako4kamiya@gmail.com>
2026-04-29 16:16:33 +0900

    固定ページ（枠組みだけでいったん終了）

---
+++ page.php
@@ -0,0 +1,58 @@
+<?php get_header(); ?>
+<main class="light-mode">
+    <section id="title">
+        <div class="container">
+            <div class="inner_container">
+                <div class="headings">
+                    <h1 class=""></h1>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="">
+        <div class="container">
+            <div class="inner_container">
+                <div class="headings">
+                    <h2 class=""></h2>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="">
+        <div class="container">
+            <div class="inner_container">
+                <div class="headings">
+                    <h2 class=""></h2>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="">
+        <div class="container">
+            <div class="inner_container">
+                <div class="headings">
+                    <h2 class=""></h2>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="">
+        <div class="container">
+            <div class="inner_container">
+                <div class="headings">
+                    <h2 class=""></h2>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="">
+        <div class="container">
+            <div class="inner_container">
+                <div class="headings">
+                    <h2 class=""></h2>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+<?php get_footer(); ?>
