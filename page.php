7f209b28459c6329701860f892eaf12acfdf6ed0
Mako Kamiya <mako4kamiya@gmail.com>
2026-04-30 14:11:31 +0900

    固定ページ About Us（枠組み）

--- page.php
+++ page.php
@@ -1,17 +1,19 @@
 <?php
     get_header();
-    $slug = get_post_field( 'post_name', get_post() );
+    $slug = get_post_field('post_name', get_the_ID());
+    $shoulder = ucwords(str_replace('-', ' ', $slug));
 ?>
-<main id="<?php echo esc_attr( $slug ); ?>" class="light-mode">
+<main id="page-<?php echo esc_attr($slug); ?>" class="light-mode">
     <section id="title">
         <div class="container">
             <div class="inner_container">
                 <div class="headings">
-                    <p class="text-style-shoulder"></p>
+                    <p class="text-style-shoulder"><?php echo esc_html($shoulder); ?></p>
                     <h1 class="text-style-h1"><?php the_title(); ?></h1>
                 </div>
             </div>
         </div>
     </section>
+    <?php get_template_part('template-parts/page', $slug); ?>
 </main>
 <?php get_footer(); ?>
