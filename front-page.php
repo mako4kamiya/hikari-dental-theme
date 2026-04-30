8f3937b97e03e619c1f0862a27a7d1febf9bde3e
Mako Kamiya <mako4kamiya@gmail.com>
2026-04-30 11:27:00 +0900

    ページ遷移はbuttonからaタグへ修正

--- front-page.php
+++ front-page.php
@@ -8,7 +8,7 @@
                     <a class="logo-mainvisual" href="<?php echo home_url(); ?>"><img src="<?php echo get_theme_file_uri(); ?>/assets/images/hikari-dental-logo-mainvisual.png" alt="ひかりデンタルクリニックのロゴメインビジュアル"></a>
                     <h1 class="text-style-main-copy">安心と信頼の<br>歯科医療</h1>
                     <p class="text-style-p-regular">一人ひとりのお悩みに真摯に向き合い、 安心して通い続けられる歯科クリニックを目指しています。</p>
-                    <button class="text-style-button" type="button">クリニック紹介を見る</button>
+                    <a class="button text-style-button" href="<?php the_permalink(14); ?>">クリニック紹介を見る</a>
                 </div>
             </div>
         </div>
@@ -21,7 +21,7 @@
                         <h2 class="text-style-h1">チームで支える歯科医療</h2>
                     </div>
                     <p class="text-style-p-regular">歯科医師・スタッフが密に連携し、一人ひとりに寄り添った診療で、安心して通い続けられる診療体制を整えています。</p>
-                    <button class="text-style-button" type="button">クリニック紹介を見る</button>
+                    <a class="button text-style-button" href="<?php the_permalink(14); ?>">クリニック紹介を見る</a>
                 </div>
                 <img class="about_us-right" src="<?php echo get_theme_file_uri(); ?>/assets/images/top-page-about-us.jpg" alt="クリニック紹介">
             </div>
