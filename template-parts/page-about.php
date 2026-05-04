101bd718d6d518ac4fe4ff49f51f522ab842e566
Mako Kamiya <mako4kamiya@gmail.com>
2026-05-04 15:08:49 +0900

    固定ページ About - greeting（完了）

--- template-parts/page-about.php
+++ template-parts/page-about.php
@@ -1,3 +1,83 @@
-<section>
-    ここはabout
+<section id="greeting">
+    <div class="container">
+        <div class="inner_container">
+            <div class="headings">
+                <h2 class="text-style-h2">院長挨拶</h2>
+            </div>
+            <div class="greeting-message">
+                <div>
+                    <p class="text-style-p-regular">
+                        はじめまして。
+                    </p>
+                    <p class="text-style-p-regular">
+                        当院では、患者さま一人ひとりのお悩みに寄り添い、安心して通い続けていただける歯科医院を目指しています。 
+                        歯科治療に対して不安や緊張を感じる方も多いため、丁寧なカウンセリングとわかりやすい説明を大切にし、納得して治療を受けていただけるよう心がけています。
+                    </p>
+                    <p class="text-style-p-regular">
+                        また、お口の健康は日々の生活の質にも大きく関わります。地域の皆さまが長く健康なお口を保てるよう、予防歯科にも力を入れ、安心して相談できる身近な歯科医院でありたいと考えています。
+                    </p>
+                    <p>
+                        お口のことで気になることがありましたら、どうぞお気軽にご相談ください。
+                    </p>
+                    <p class="text-style-p-regular">
+                        皆さまのご来院を心よりお待ちしております。
+                    </p>
+                </div>
+                <img src="<?php echo get_theme_file_uri(); ?>/assets/images/about-page-hikaru-hoshino.jpg   " alt="院長星野ひかるの顔写真">
+            </div>
+            <div class="director-history">
+                <h3 class="text-style-p-bold">院長　星野 ひかる</h3>
+                <dl class="text-style-p-regular">
+                    <div class="row">
+                        <dt>2010年</dt><dd>○○大学歯学部　卒業</dd>
+                    </div>
+                    <div class="row">
+                        <dt>2011年</dt><dd>○○大学歯学部附属病院　臨床研修 修了</dd>
+                    </div>
+                    <div class="row">
+                        <dt>2012年</dt><dd>医療法人〇〇会 ○○歯科クリニック　勤務</dd>
+                    </div>
+                    <div class="row">
+                        <dt>2026年</dt><dd>ひかりデンタルクリニック　開院</dd>
+                    </div>
+                </dl>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="staff">
+    <div class="container">
+        <div class="inner_container">
+            <div class="headings">
+                <h2 class="text-style-h2"></h2>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="facilities">
+    <div class="container">
+        <div class="inner_container">
+            <div class="headings">
+                <h2 class="text-style-h2"></h2>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="clinic-hours">
+    <div class="container">
+        <div class="inner_container">
+            <div class="headings">
+                <h2 class="text-style-h2"></h2>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="breadcrumb">
+    <div class="container">
+        <div class="inner_container">
+            <div class="headings">
+                <h2 class="text-style-h2"></h2>
+            </div>
+        </div>
+    </div>
 </section>
