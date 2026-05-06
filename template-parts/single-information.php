4532989ef595a402a565777655e7ea97d68a6e21
Mako Kamiya <mako4kamiya@gmail.com>
2026-05-06 14:25:18 +0900

    カスタム投稿タイプ Information の追加（完了）

--- template-parts/single-information.php
+++ template-parts/single-information.php
@@ -1,1 +1,9 @@
-あああああ
+<div class="contents">
+    <?php the_post_thumbnail(); ?>
+    <div class="container">
+        <div class="inner_container">
+            <?php the_excerpt(); ?>
+            <?php the_content(); ?>
+        </div>
+    </div>
+</div>
