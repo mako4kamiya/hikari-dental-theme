c36b6ab9bfc3f976765af8c91218cb75cfac4a04
Mako Kamiya <mako4kamiya@gmail.com>
2026-05-15 11:15:30 +0900

    お知らせの一覧ページ（完了）

---
+++ category.php
@@ -0,0 +1,31 @@
+<?php
+    get_header();
+    $post_type_obj = get_queried_object();
+    $post_type_name = $post_type_obj->name;
+?>
+<main id="category-<?php echo esc_attr($post_type_name); ?>" class="light-mode">
+    <?php get_template_part('template-parts/header-entry'); ?>
+
+    <section>
+        <div class="container">
+            <div class="inner_container">
+                <?php if (have_posts()) : ?>
+                    <ul>
+                    <?php while (have_posts()) : the_post(); ?>
+                        <li id="<?php the_ID(); ?>">
+                            <a href="<?php the_permalink(); ?>" class="text-style-a-regular">
+                                <?php echo get_the_date('Y.m.d'); ?>　<?php the_title(); ?>
+                            </a>
+                        </li>
+                    <?php endwhile; ?>
+                    </ul>
+                <?php else : ?>
+                    <p class="news-no-posts-text">現在、お知らせはございません。</p>
+                <?php endif; ?>
+            </div>
+        </div>
+    </section>
+
+    <?php get_template_part('template-parts/breadcrumb'); ?>
+</main>
+<?php get_footer(); ?>
