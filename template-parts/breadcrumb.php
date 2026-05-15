cd9848f899176e915268ea1812d6a8ce0a85b708
Mako Kamiya <mako4kamiya@gmail.com>
2026-05-15 11:27:56 +0900

    パンくずリストの二つ目のリストを条件を指定して非表示にした

--- template-parts/breadcrumb.php
+++ template-parts/breadcrumb.php
@@ -4,7 +4,9 @@
             <nav aria-label="パンくずリスト">
                 <ol class="breadcrumb-list">
                     <li><a class="text-style-a-regular" href="<?php echo home_url(); ?>">ホーム</a></li>
-                    <li class="text-style-p-regular" aria-current="page"><?php the_title(); ?></li>
+                    <?php if ( !is_archive() && !is_category() && have_posts() ) : ?>
+                        <li class="text-style-p-regular" aria-current="page"><?php the_title(); ?></li>
+                    <?php endif; ?>
                 </ol>
             </nav>
         </div>
