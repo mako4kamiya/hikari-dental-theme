23c61b61de4e8101b78d3732b43d1b82ec21fc1d
Mako Kamiya <mako4kamiya@gmail.com>
2026-05-19 14:56:46 +0900

    プライバシーポリシーページ

---
+++ page-privacy-policy.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+<main class="light-mode">
+    <?php get_template_part('template-parts/header-entry'); ?>
+    
+    <section id="privacy-policy" class="light-mode">
+        <div class="container">
+            <div class="inner_container">
+                <?php the_content(); ?>
+            </div>
+        </div>  
+    </section>
+
+    <?php get_template_part('template-parts/breadcrumb'); ?>
+</main>
+
+<?php get_footer(); ?>
