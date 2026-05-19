5fe9aaac37838580a28907a71fa528b757925299
Mako Kamiya <mako4kamiya@gmail.com>
2026-05-19 13:26:34 +0900

    アクセスページ

---
+++ page-contact.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+
+<main class="light-mode">
+    <?php get_template_part('template-parts/header-entry'); ?>
+    
+    <section id="contact" class="light-mode">
+        <?php the_post_thumbnail(); ?>
+        <div class="container">
+            <div class="inner_container">
+                <div class="contact-form-7">
+                    <?php echo do_shortcode( '[contact-form-7 id="f46cafd" title="コンタクトフォーム"]' ); ?>
+                </div>
+                <div class="content">
+                    <?php the_content(); ?>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php get_template_part('template-parts/breadcrumb'); ?>
+</main>
+
+<?php get_footer(); ?>
