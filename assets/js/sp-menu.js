fc0fb944107f5d11062ebe2ed24665aa5e35e38e
Mako Kamiya <mako4kamiya@gmail.com>
2026-05-25 13:05:28 +0900

    スマホサイズ メニューの開閉 完了

--- assets/js/sp-menu.js
+++ assets/js/sp-menu.js
@@ -1,6 +1,8 @@
 const SpMenu = document.querySelector('.sp-menu');
+const headerNav = document.querySelector('nav#header-nav');
 const HeaderIcon = document.querySelector('.header-icon');
 
 SpMenu.addEventListener('click', () => {
     HeaderIcon.classList.toggle('is-active');
+    headerNav.classList.toggle('is-active');
 });
